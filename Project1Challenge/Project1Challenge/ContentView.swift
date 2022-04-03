390edbb6973841c3336e1453ff3851c822aac34f
citixenken <kmuyesu@gmail.com>
2022-04-03 18:38:50 +0300

    setting up the navigation form and sections

--- Project1Challenge/Project1Challenge/ContentView.swift
+++ Project1Challenge/Project1Challenge/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputUnit = 0.0
+    @State private var defaultConversion = ["Minutes", "Hours", "Days"]
+//    enum conversionFormats {
+//        case minutes, hours, days
+//    }
+    var outputUnit: Double {
+        return -1
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter value in seconds", value: $inputUnit, format: .number)
+                    
+                } header: {
+                    Text("Input in seconds")
+                }
+                
+                Section {
+                    Picker("Convert to:", selection: $defaultConversion) {
+                        ForEach(defaultConversion, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                } header: {
+                    Text("select unit to convert input to")
+                }
+                .pickerStyle(.segmented)
+                
+                Section {
+                    Text(outputUnit, format: .number)
+                    
+                } header: {
+                    Text("Output")
+                }
+                
+            }
+            .navigationTitle("Time Converter")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
