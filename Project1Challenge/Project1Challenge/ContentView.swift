075f2ba611b75dbd61b46772bf114d675e17fcb5
citixenken <kmuyesu@gmail.com>
2022-04-03 17:44:55 +0300

    project 1 initial commit

---
+++ Project1Challenge/Project1Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project1Challenge
+//
+//  Created by Ken Muyesu on 03/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project1Challenge/Project1Challenge/Project1ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project1ChallengeApp.swift
+//  Project1Challenge
+//
+//  Created by Ken Muyesu on 03/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Project1ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
