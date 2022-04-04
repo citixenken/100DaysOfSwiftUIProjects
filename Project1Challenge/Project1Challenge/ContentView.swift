4fe4e3111cb9ddd9ec72fc944615c0e9f9cba42b
citixenken <kmuyesu@gmail.com>
2022-04-04 11:57:23 +0300

    implementing length converter functionality

--- Project1Challenge/Project1Challenge/ContentView.swift
+++ Project1Challenge/Project1Challenge/ContentView.swift
@@ -8,46 +8,110 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var inputUnit = 0.0
-    @State private var defaultConversion = ["Minutes", "Hours", "Days"]
-//    enum conversionFormats {
-//        case minutes, hours, days
-//    }
-    var outputUnit: Double {
-        return -1
+    
+    enum DefaultConversion: String, CaseIterable {
+        case metres = "M"
+        case kilometres = "KM"
+        case feet = "Ft"
+        case yards = "Yds"
+        case miles = "Mil"
+    }
+    
+    @State private var inputValue: DefaultConversion = .metres
+    @State private var outputValue: DefaultConversion = .metres
+    @State private var input = ""
+    
+    //handling input focus for our UI
+    @FocusState private var amountIsFocused: Bool
+    
+    var lengthConverter: Double {
+        guard let inputAmount = Double(input) else { return 0 }
+        //let inputAmount = Double(input)
+        var conversionToMetres = 0.0
+        var conversionResult = 0.0
+        
+        //converting all input types to metres
+        switch inputValue {
+        case .metres:
+            conversionToMetres = inputAmount * 1
+        case .kilometres:
+            conversionToMetres = inputAmount * 1000
+        case .feet:
+            conversionToMetres = inputAmount * 0.3048
+        case .yards:
+            conversionToMetres = inputAmount * 0.9144
+        case .miles:
+            conversionToMetres = inputAmount * 1609.344
+        }
+        
+        //converting to desired output unit
+        switch outputValue {
+        case .metres:
+            conversionResult = conversionToMetres * 1
+        case .kilometres:
+            conversionResult = conversionToMetres * 0.001
+        case .feet:
+            conversionResult = conversionToMetres * 3.28084
+        case .yards:
+            conversionResult = conversionToMetres * 1.093613
+        case .miles:
+            conversionResult = conversionToMetres * 0.0006213712
+        }
+        
+        return conversionResult
     }
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Enter value in seconds", value: $inputUnit, format: .number)
-                    
-                } header: {
-                    Text("Input in seconds")
-                }
-                
-                Section {
-                    Picker("Convert to:", selection: $defaultConversion) {
-                        ForEach(defaultConversion, id: \.self) {
-                            Text("\($0)")
+                    Picker("Convert:", selection: $inputValue) {
+                        ForEach(DefaultConversion.allCases, id: \.self) {
+                            Text($0.rawValue)
                         }
                     }
                 } header: {
-                    Text("select unit to convert input to")
+                    Text("select input unit")
                 }
                 .pickerStyle(.segmented)
                 
                 Section {
-                    Text(outputUnit, format: .number)
+                    TextField("Enter value", text: $input)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                } header: {
+                    Text("Input")
+                }
+
+                Section {
+                    Picker("Convert:", selection: $outputValue) {
+                        ForEach(DefaultConversion.allCases, id: \.self) {
+                            Text($0.rawValue)
+                        }
+                    }
+                } header: {
+                    Text("select output unit")
+                }
+                .pickerStyle(.segmented)
+                
+                Section {
+                    Text(lengthConverter, format: .number)
                     
                 } header: {
                     Text("Output")
                 }
-                
             }
-            .navigationTitle("Time Converter")
+            .navigationTitle("Length Converter")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
