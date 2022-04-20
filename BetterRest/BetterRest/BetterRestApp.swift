19b5badf77ad7a791980ede9c62cff5b251280ee
citixenken <kmuyesu@gmail.com>
2022-04-20 10:59:26 +0300

    setting up BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ken Muyesu on 20/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
