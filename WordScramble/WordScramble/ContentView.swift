87fd11bb138a3362bf8dddfbd4f3e68ccc242db4
citixenken <kmuyesu@gmail.com>
2022-04-25 21:19:26 +0300

    adding to a list of words

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word:", text: $newWord)
+                        .autocapitalization(.none)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle.fill")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .navigationBarTitleDisplayMode(.inline)
+            .onSubmit(addNewWord)
+        }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        //more extra validation techniques
+        
+        withAnimation {
+            //insert at start of the array
+            usedWords.insert(answer, at: 0)
+        }
+        
+        newWord = ""
     }
 }
 
