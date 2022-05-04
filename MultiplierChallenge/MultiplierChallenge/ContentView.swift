7dd4b4936d85ebf7eab9b52fb062c6863516eb8e
citixenken <kmuyesu@gmail.com>
2022-05-04 21:52:30 +0300

    MultiplierChallenge initial setup

---
+++ MultiplierChallenge/MultiplierChallenge/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MultiplierChallenge
+//
+//  Created by Ken Muyesu on 04/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ MultiplierChallenge/MultiplierChallenge/MultiplierChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplierChallengeApp.swift
+//  MultiplierChallenge
+//
+//  Created by Ken Muyesu on 04/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiplierChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
