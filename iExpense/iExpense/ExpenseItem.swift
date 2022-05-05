1270d7b7c3971fd152dd5faa1cc82fa595b104e1
citixenken <kmuyesu@gmail.com>
2022-05-05 14:39:15 +0300

    building a list we can delete from

---
+++ iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,8 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Ken Muyesu on 05/05/2022.
+//
+
+import Foundation
---
+++ iExpense/iExpense/Expenses.swift
@@ -0,0 +1,8 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by Ken Muyesu on 05/05/2022.
+//
+
+import Foundation
