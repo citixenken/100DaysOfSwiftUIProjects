a2e5602de724a4695980610126e5e68461f8284f
citixenken <kmuyesu@gmail.com>
2022-05-05 09:37:06 +0300

    barebones

--- MultiplierChallenge/MultiplierChallenge/ContentView.swift
+++ MultiplierChallenge/MultiplierChallenge/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputA = 1
+    @State private var inputB = 1
+    @State private var outputAnswer = 1
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Picker("\(inputA.formatted())", selection: $inputA) {
+                        ForEach(0..<11) {
+                            Text("\($0)")
+                        }
+                    }
+                } header: {
+                    Text("Enter first param")
+                }
+                Section {
+                    Picker("\(inputB.formatted())", selection: $inputB) {
+                        ForEach(0..<11) {
+                            Text("\($0)")
+                        }
+                    }
+                } header: {
+                    Text("Enter second param")
+                }
+                Section {
+                    Text(calculatedOutput())
+                } header: {
+                    Text("Output")
+                }
+            }
+            .navigationTitle("Multiplier")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+    
+    private func calculatedOutput() -> String {
+        return String(inputA * inputB)
     }
 }
 
