1d04fb8820a453c9c95e0e84ddac204b8d899d32
citixenken <kmuyesu@gmail.com>
2022-05-05 10:24:39 +0300

    iExpense setup

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ken Muyesu on 05/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
