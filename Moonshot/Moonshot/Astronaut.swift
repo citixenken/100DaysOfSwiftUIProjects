3e7d5ab145f5aa488f0280fc3cc7fb56618d2921
citixenken <kmuyesu@gmail.com>
2022-05-06 14:19:30 +0300

    loading Codable data

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Ken Muyesu on 06/05/2022.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    //var id = UUID()
+    let id: String
+    let name: String
+    let description: String
+}
