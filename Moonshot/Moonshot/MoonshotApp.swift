cae36c675099d5635e8d804cbe16036217d7a223
citixenken <kmuyesu@gmail.com>
2022-05-06 12:28:16 +0300

    setting up Moonshot

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ken Muyesu on 06/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
