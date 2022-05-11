329717b300e5d19c6ad452b25558abb8cab01a73
citixenken <kmuyesu@gmail.com>
2022-05-11 19:30:30 +0300

    initial commit

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Ken Muyesu on 11/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
