cf0aa4069ac4f2f8cd45e781aa68e7807b032457
citixenken <kmuyesu@gmail.com>
2022-05-13 16:02:43 +0300

    encoding an ObservableObject class

--- CupcakeCorner/CupcakeCorner/Order.swift
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -9,7 +9,13 @@
 
 import SwiftUI
 
-class Order: ObservableObject {
+class Order: ObservableObject, Codable {
+    
+    //@Published properties
+    enum CodingKeys: CodingKey {
+        case type, quantity, extraFrosting, addSprinkles, name, streetAddress, city, zip
+    }
+    
     static let types = ["Vanilla", "Mint", "Strawberry", "Chocolate"]
     
     @Published var type = 3
@@ -59,5 +65,40 @@ class Order: ObservableObject {
         }
         
         return cost
+    }
+    //empty init with NO params
+    init() {}
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        //type, quantity, extraFrosting, addSprinkles, name, streetAddress, city, zip
+        try container.encode(type, forKey: .type)
+        try container.encode(quantity, forKey: .quantity)
+        
+        try container.encode(extraFrosting, forKey: .extraFrosting)
+        try container.encode(addSprinkles, forKey: .addSprinkles)
+        
+        try container.encode(name, forKey: .name)
+        try container.encode(streetAddress, forKey: .streetAddress)
+        try container.encode(city, forKey: .city)
+        try container.encode(zip, forKey: .zip)
+
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        type = try container.decode(Int.self, forKey: .type)
+        quantity = try container.decode(Int.self, forKey: .quantity)
+
+        extraFrosting = try container.decode(Bool.self, forKey: .extraFrosting)
+        addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
+
+        name = try container.decode(String.self, forKey: .name)
+        streetAddress = try container.decode(String.self, forKey: .streetAddress)
+        city = try container.decode(String.self, forKey: .city)
+        zip = try container.decode(String.self, forKey: .zip)
+
     }
 }
