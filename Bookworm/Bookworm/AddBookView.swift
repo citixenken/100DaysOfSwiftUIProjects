73b2b3d10507026f02c8f62d1eec43029437ac8d
citixenken <kmuyesu@gmail.com>
2022-05-16 19:31:42 +0300

    creating books instance with Core Data

---
+++ Bookworm/Bookworm/AddBookView.swift
@@ -0,0 +1,79 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Ken Muyesu on 16/05/2022.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 4
+    @State private var genre = ""
+    @State private var review = ""
+    
+    let genres = ["Romance", "Fiction", "Fantasy", "Thriller", "Poetry", "Non-Fiction", "History", "Mystery"]
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+                    
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                } header: {
+                    Text("Book Details")
+                }
+                
+                Section {
+                    TextEditor(text: $review)
+                    
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text(String($0))
+                            
+                        }
+                    }
+                } header: {
+                    Text("Write a Book Review")
+                }
+                
+                Section {
+                    Button("Save") {
+                        //add the book
+                        let newBook = Book(context: moc)
+                        newBook.id = UUID()
+                        newBook.title = title
+                        newBook.author = author
+                        newBook.rating = Int16(rating)
+                        newBook.genre = genre
+                        newBook.review = review
+                        
+                        try? moc.save()
+                        
+                        //dismiss sheet presentation after save
+                        dismiss()
+                    }
+                }
+            }
+            .navigationTitle("Add Book")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
