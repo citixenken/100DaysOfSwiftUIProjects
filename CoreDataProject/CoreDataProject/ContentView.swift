48552fad2ad1ceb179e077526467bde254c96d52
citixenken <kmuyesu@gmail.com>
2022-05-19 13:45:44 +0300

    revisions

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: [], predicate: nil) var ships: FetchedResults<Ship>
+    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe IN %@", ["Star Wars", "Aliens", "Firefly"])) var ships: FetchedResults<Ship>
     
     var body: some View {
         VStack {
@@ -22,21 +22,22 @@ struct ContentView: View {
                 let shipOne = Ship(context: moc)
                 shipOne.name = "Enterprise"
                 shipOne.universe = "Star Trek"
-                
+
                 let shipTwo = Ship(context: moc)
                 shipTwo.name = "Defiant"
                 shipTwo.universe = "Star Trek"
-                
+
                 let shipThree = Ship(context: moc)
                 shipThree.name = "Millenium Falcon"
                 shipThree.universe = "Star Wars"
-                
+
                 let shipFour = Ship(context: moc)
                 shipFour.name = "Executor"
-                shipFour.universe = "Star Trek"
-                
+                shipFour.universe = "Star Wars"
+
                 try? moc.save()
             }
+
         }
     }
 }
