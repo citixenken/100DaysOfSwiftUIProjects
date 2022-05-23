9053b71b69bf7904eccdf63d8d1a7c9887f0ebe1
citixenken <kmuyesu@gmail.com>
2022-05-23 22:40:28 +0300

    Integrating CoreImage with SwiftUI

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,35 +5,60 @@
 //  Created by Ken Muyesu on 20/05/2022.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
     
     var body: some View {
-        NavigationView {
-            VStack {
-                Text("Tap here for The Rainbow Experience!")
-                    .frame(width: 350, height: 350)
-                    .background(backgroundColor)
-                    .onTapGesture {
-                        showingConfirmation = true
-                    }
-                    .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                        Button("Red") { backgroundColor = .red }
-                        Button("Orange") { backgroundColor = .orange }
-                        Button("Yellow") { backgroundColor = .yellow }
-                        Button("Green") { backgroundColor = .green }
-                        Button("Blue") { backgroundColor = .blue }
-                        Button("Indigo") { backgroundColor = .indigo }
-                        Button("Purple") { backgroundColor = .purple }
-                        Button("Cancel", role: .cancel) { }
-                    } message: {
-                        Text("Select a new color")
-                    }
-            }
-            .navigationTitle("Rainbow Colours")
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+//        image = Image("amvalkyrie")
+        guard let inputImage = UIImage(named: "amvalkyrie") else { return }
+        
+        //CIImage?
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+//        let currentFilter = CIFilter.sepiaTone()
+        let currentFilter = CIFilter.crystallize() //.pixellate(); .bokehblur(); .twirlDistortion()...
+        currentFilter.inputImage = beginImage
+//        currentFilter.intensity = 1
+//        currentFilter.radius = 800
+//        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
+        
+        //handle inputs dynamically
+        let amount = 1.0
+        let inputKeys = currentFilter.inputKeys
+        
+        
+        //inject key; if supported
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
+        }
+        
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(amount * 60, forKey: kCIInputRadiusKey)
+        }
+        
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(amount * 40, forKey: kCIInputScaleKey)
+        }
+        
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
         }
     }
 }
