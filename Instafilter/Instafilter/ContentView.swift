d03e12b2049fd2efb98ed0b8d94bf1e4f891aab1
citixenken <kmuyesu@gmail.com>
2022-05-24 09:26:13 +0300

    Using coordinators to manage SwiftUI view controllers

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var image: Image?
+    @State private var inputImage: UIImage?
     @State private var showingImagePicker = false
     
     var body: some View {
@@ -27,8 +28,18 @@ struct ContentView: View {
             .cornerRadius(16)
         }
         .sheet(isPresented: $showingImagePicker) {
-            ImagePicker()
+            ImagePicker(image: $inputImage)
         }
+        .onChange(of: inputImage) { _ in loadImage() }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else {
+            return
+        }
+        
+        image = Image(uiImage: inputImage)
+
     }
 }
 
--- Instafilter/Instafilter/ImagePicker.swift
+++ Instafilter/Instafilter/ImagePicker.swift
@@ -9,6 +9,29 @@ import PhotosUI
 import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            
+            guard let provider = results.first?.itemProvider else { return }
+            
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+        }
+    }
+    
     func makeUIViewController(context: Context) -> PHPickerViewController {
         //pick your choice
         var config = PHPickerConfiguration()
@@ -17,10 +40,19 @@ struct ImagePicker: UIViewControllerRepresentable {
         
         //load choice into viewcontroller
         let picker = PHPickerViewController(configuration: config)
+        
+        //use coordinator as our delegate for phpicker
+        picker.delegate = context.coordinator
+        
         return picker
     }
     
     func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
         //leave it empty!!!
+    }
+    
+    //communicates changes from viewcontroller to other instances of swiftui
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
     }
 }
