1c1f5f9efcf000bd1684dbe07fada0763571c921
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-03 20:58:23 +0300

    initial BucketList setup

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Ken Muyesu on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
